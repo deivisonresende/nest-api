5920a28c56d85e8f20d054d6ebcead9af74202b7
Deivison Resende <deivison4844@gmail.com>
2024-08-24 12:30:25 -0300

    feat:Setup environment vars using nestjs configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
 import { CreateAccountController } from './controllers/create-account.controller';
 import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true
+    })
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService]
 })
+
 export class AppModule { }
